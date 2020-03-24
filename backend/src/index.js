d8f9b109cea1084c77b870787da01ce3537a64bb
Edson Rodrigo <edson_nt2@hotmail.com>
2020-03-24 17:24:44 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const Routes = require("./routes");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(Routes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Node conected in Port ${PORT}`));
---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const routes = require("express").Router();
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+routes.delete("/ongs", OngController.delete);
+
+routes.get("/profile", ProfileController.index);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.post("/sessions", SessionController.create);
+
+module.exports = routes;
